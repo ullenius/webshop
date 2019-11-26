4cd5d41f91f2cd9092f4764074d365a9c4d3ec0d
Anosh D. Ullenius <anosh@anosh.se>
2019-11-26 08:56:19 +0100

    Clean up imports

--- src/main/java/se/anosh/webshop/domain/Product.java
+++ src/main/java/se/anosh/webshop/domain/Product.java
@@ -2,13 +2,12 @@ package se.anosh.webshop.domain;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Collection;
+
 import javax.persistence.Basic;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlRootElement;
