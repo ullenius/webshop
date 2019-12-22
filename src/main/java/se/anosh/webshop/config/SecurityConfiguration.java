10a881dba86e7ae51682ef7361b98d9d2b7e736b
Anosh D. Ullenius <anosh@anosh.se>
2019-12-22 19:36:46 +0100

    Bcrypt is working :)

--- src/main/java/se/anosh/webshop/config/SecurityConfiguration.java
+++ src/main/java/se/anosh/webshop/config/SecurityConfiguration.java
@@ -3,12 +3,15 @@ package se.anosh.webshop.config;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -17,10 +20,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter  {
 	@Autowired
     DataSource dataSource;
 
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
 	//Enable jdbc authentication
     @Autowired
     public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication().dataSource(dataSource);
+        auth.jdbcAuthentication().dataSource(dataSource).passwordEncoder(passwordEncoder());
     }
     
     @Override
@@ -31,7 +39,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter  {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-    	http.authorizeRequests().antMatchers("/","/addUser","/saveUser").permitAll().antMatchers("/welcome").hasAnyRole("USER", "ADMIN")
+    	http.authorizeRequests().antMatchers("/","/addUser","/saveUser","/login.html","/css/*","/menu.html").permitAll().antMatchers("/welcome").hasAnyRole("USER", "ADMIN")
     	.antMatchers("/getEmployees").hasAnyRole("USER", "ADMIN").antMatchers("/addNewEmployee")
     	.hasAnyRole("ADMIN").anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll()
     	.and().logout().permitAll();
