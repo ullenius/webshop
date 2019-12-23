9498ddab9e73d3ad48459c82c9cae709012af6f3
Anosh D. Ullenius <anosh@anosh.se>
2019-12-23 10:27:23 +0100

    Display order details after submitting order

--- src/main/java/se/anosh/webshop/controller/ShoppingCartController.java
+++ src/main/java/se/anosh/webshop/controller/ShoppingCartController.java
@@ -3,6 +3,7 @@ package se.anosh.webshop.controller;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -11,13 +12,12 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
 
 import se.anosh.webshop.dao.exception.ProductNotFoundException;
+import se.anosh.webshop.domain.Orderline;
 import se.anosh.webshop.domain.Product;
 import se.anosh.webshop.service.OrderService;
 import se.anosh.webshop.service.ProductService;
@@ -101,11 +101,12 @@ public class ShoppingCartController {
 	public ModelAndView orderMapping() {
 		
 		try {
-			int orderId = submitOrder(); // return the order number?
+			final int orderId = submitOrder();
+			List<Orderline> orderLines = orderService.findMatchingOrderlines(orderId); // duplicate code from orderDetails (adminController)
+			return new ModelAndView("orderdetails","model",orderLines);
 		} catch (IllegalStateException ex) {
 			return Redirect.error();
 		}
-		return Redirect.success(); // TODO: need the order # to print out the details
 	}
 	
 	private int submitOrder() { // TODO: OptionalInt?
